2633bd4ace054d7d8843539afce59d5d93670d0b
Ian Chelaru <ian.chelaru@frequentis.com>
2020-08-07 10:24:25 +0300

    test for getter

---
+++ ide/src/main/java/com/training/ide/IdeTipsAndTricks.java
@@ -0,0 +1,17 @@
+package com.training.ide;
+
+public class IdeTipsAndTricks {
+    private String myProp;
+
+    public IdeTipsAndTricks(String myProp) {
+        this.myProp = myProp;
+    }
+
+    public String getMyProp() {
+        return myProp;
+    }
+
+    public void setMyProp(String myProp) {
+        this.myProp = myProp;
+    }
+}
