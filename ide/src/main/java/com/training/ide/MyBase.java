77e50fb6e7320238c116dc8506f8bc5373c63d33
SERBAN Mihai-Ioan <Mihai-Ioan.SERBAN@frequentis.com>
2020-08-07 11:18:15 +0300

    initial pom

---
+++ ide/src/main/java/com/training/ide/MyBase.java
@@ -0,0 +1,4 @@
+package com.training.ide;
+
+public class MyBase {
+}
---
+++ mars/src/main/java/com/training/mars/Coordinate.java
@@ -0,0 +1,21 @@
+package com.training.mars;
+
+public class Coordinate {
+    private Double lon;
+    private Double lat;
+
+    public Coordinate(Double lon, Double lat) {
+        this.lon = lon;
+        this.lat = lat;
+    }
+
+    public Double getLon() {
+        return lon;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+
+}
